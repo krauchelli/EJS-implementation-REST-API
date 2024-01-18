2b9b6dbe62da84feb154407f78d44dc5863e88f5
krauchelli <mekaku26shi@gmail.com>
2024-01-18 15:16:33 +0700

    Adding new route

--- routes.js
+++ routes.js
@@ -14,7 +14,12 @@ router.get('/products', async (req, res) => {
   const products = await Product.find({});
   res.render('products/index', { products });
 });
+router.get('/products/create', (req, res) => {
+  res.render('products/create');
+});
 
+// get specific product
+// pastikan posisi routing ini di paling bawah supaya routing lain tidak dianggap sebagai query parameter
 router.get('/products/:id', async (req, res) => {
   const product = await Product.findById(req.params.id);
   res.render('products/show', { product });
