1921bd51b09734f816aded72e79ebc262adcf02b
krauchelli <mekaku26shi@gmail.com>
2024-01-18 08:15:13 +0700

    second commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override');
 const express = require('express');
 const mongoose = require('mongoose');
 const { router } = require('./routes');
+const { Product } = require('./models/product');
 
 const app = express();
 
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { Product } = require('./models/product');
 
 const router = express.Router();
 
