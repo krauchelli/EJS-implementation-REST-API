50dab45259d97aeb7b16c4bbede70e082169ad8e
krauchelli <mekaku26shi@gmail.com>
2024-01-18 14:56:36 +0700

    Adding new routes to the index

--- routes.js
+++ routes.js
@@ -9,3 +9,13 @@ module.exports = { router };
 router.get('/', (req, res) => {
   res.send('Hello world!');
 });
+
+router.get('/products', async (req, res) => {
+  const products = await Product.find({});
+  res.render('products/index', { products });
+});
+
+router.get('/products/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.render('products/show', { product });
+});
